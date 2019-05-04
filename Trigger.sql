48cd35a26887ce2d3fa3480e597a79a2403b0eaf
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-04 16:14:14 +0200

    Add files via upload

--- Trigger.sql
+++ Trigger.sql
@@ -1,3 +1,6 @@
+drop trigger ratingcheck;
+drop function contador;
+drop procedure contar;
 delimiter //
 create TRIGGER ratingcheck BEFORE INSERT ON aboutdisks FOR EACH ROW IF NEW.rating >10 THEN SET NEW.rating = 0; 
 END IF;//
@@ -7,13 +10,13 @@ create function contador()
 returns varchar(20)
 begin
 	declare musname varchar(20);                               
-	select name into musname from musician;
+	select count(name) into musname from musician;
     return musname;
 end;//
 
 delimiter //
 create procedure contar(out musname varchar(20))
 begin
- select name into musname from musician;
+ select count(name) into musname from musician;
  end;
  //
