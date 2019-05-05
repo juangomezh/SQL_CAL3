9aaca7719494c2923c2424130b17b107f06a2552
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-05 23:00:16 +0200

    Add files via upload

--- CreateUser.sql
+++ CreateUser.sql
@@ -1,7 +1,9 @@
 drop user if exists boss;
 drop user if exists customer;
+/*boss user*/
 create user boss;
 grant insert, update, delete on assigment.concerts to boss;
+/*customer/*
 create user customer;
 grant select on assigment.concerts to customer;
 grant select on assigment.buydisks to customer;
--- Trigger.sql
+++ Trigger.sql
@@ -1,10 +1,11 @@
 drop trigger if exists ratingcheck;
 drop function if exists contador;
 drop procedure if exists contar;
+/*trigger*/
 delimiter //
-create TRIGGER ratingcheck BEFORE INSERT ON aboutdisks FOR EACH ROW IF NEW.rating >10 THEN SET NEW.rating = 0; 
+create TRIGGER ratingcheck BEFORE INSERT ON aboutdisks FOR EACH ROW IF NEW.rating >10 THEN SET NEW.rating = 10; 
 END IF;//
-
+/*function*/
 delimiter //
 create function contador()
 returns varchar(20)
@@ -13,10 +14,13 @@ begin
 	select count(name) into musname from musician;
     return musname;
 end;//
-
+/*procedure*/
 delimiter //
 create procedure contar(out musname varchar(20))
 begin
  select count(name) into musname from musician;
- end;
- //
+ end;//
+
+/**calling the functions*/
+call contar(@musname);
+select contador();
