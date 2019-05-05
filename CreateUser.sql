28d21ba1e5b637dd605fef71b72e1ca7aeefe5dc
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-05 18:47:38 +0200

    Add files via upload

--- CreateUser.sql
+++ CreateUser.sql
@@ -4,3 +4,4 @@ create user boss;
 grant insert, update, delete on assigment.concerts to boss;
 create user customer;
 grant select on assigment.concerts to customer;
+grant select on assigment.buydisks to customer;
